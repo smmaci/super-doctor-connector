5419c3e8b7a94f62c3872bd3d7094f7110c95e92
cheneymah18 <cheney.lyford@gmail.com>
2016-11-21 17:59:44 -0500

    Update DoctorMap.java

--- src/DoctorMap.java
+++ src/DoctorMap.java
@@ -11,6 +11,7 @@ public class DoctorMap {
 		map.put("Leg", new Doctor("John", "123 Leg Rd", "555-555-5555", "legs"));
 		map.put("Arm", new Doctor("James", "456 Arm Ave", "111-111-1111", "arms"));
 		map.put("Head", new Doctor("Jim", "789 Head St", "222-222-2222", "heads"));
+		map.put("Skin", new Doctor("Bob", "876 Skin Blvd", "123-456-789", "skin"));
 	}
 
 }

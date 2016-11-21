c66fd23260af9bc31fe1173406b445f9179b7e5f
cheneymah18 <cheney.lyford@gmail.com>
2016-11-21 18:02:37 -0500

    Update Problem.java

--- src/Problem.java
+++ src/Problem.java
@@ -10,6 +10,12 @@ public class Problem {
 		symptoms = new String[5];
 	}
 	
+	public Problem(boolean b, int i) {
+		illness = b;
+		symptoms = new String[i];
+	}
+		
+	
 	public void pickSymptoms(){
 		
 	}

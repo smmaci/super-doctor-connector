2cbe3d5e1e9f57b521310c3d6484055b88db6b3e
Bryan James Lloyd <bryanlloyd4293@gmail.com>
2016-11-21 17:49:11 -0500

    Added default constructor to DoctorMap.java.

--- src/Doctor.java
+++ src/Doctor.java
@@ -14,7 +14,19 @@ public class Doctor {
 		field = "";
 	}
 	
-	public void viwSymptoms(){
+	public Doctor(String n, String a, String p, String f) {
+		name = n;
+		address = a;
+		phoneNumber = p;
+		field = f;
+	}
+	
+	public String toString() {
+		return ("Name: " + name + "\nAddress: " + address +
+				"\nPhone Number: " + phoneNumber + "\nField: " + field);
+	}
+	
+	public void viewSymptoms(){
 		
 	}
 	
--- src/DoctorMap.java
+++ src/DoctorMap.java
@@ -1,3 +1,16 @@
+import java.lang.String;
+import java.util.*;
+
 public class DoctorMap {
+	
+	private HashMap<String, Doctor> map;
+	
+	public DoctorMap() {
+		map = new HashMap<String, Doctor>();
+		
+		map.put("Leg", new Doctor("John", "123 Leg Rd", "555-555-5555", "legs"));
+		map.put("Arm", new Doctor("James", "456 Arm Ave", "111-111-1111", "arms"));
+		map.put("Head", new Doctor("Jim", "789 Head St", "222-222-2222", "heads"));
+	}
 
 }

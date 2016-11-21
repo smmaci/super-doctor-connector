2a3f09bbfeb0e63b55e5f0dc00df2830dc860875
Sam Mallamaci <sam.mallamaci@gmail.com>
2016-11-21 18:40:28 -0500

    updated name from UI to UserInterface

--- src/Runner.java
+++ src/Runner.java
@@ -6,7 +6,7 @@ public class Runner {
 	public static void main(String[] args) {
 		
 		DoctorMap dm = new DoctorMap();
-		
+		UserInterface main = new UserInterface();
 		System.out.println(dm.map.get("Leg").toString());
 	}
 	

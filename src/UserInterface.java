3d2747f2d6fdfc637a8efd47936aa9cacda3fe7e
cheneymah18 <cheney.lyford@gmail.com>
2016-11-21 20:01:05 -0500

    Update UserInterface.java

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -3,19 +3,20 @@ import javax.swing.border.TitledBorder;
 
 import java.awt.*;
 
-public class UserInterface {
-	public UserInterface(){
+public class UI {
+	public UI(){
 		//Set up main frame
 		JFrame main = new JFrame("Super Doctor Connector");
 		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		main.setSize(800, 600);
+		main.setSize(500, 500);
 		
 		//Set up patient panel
-		JPanel patientPanel = new JPanel();
+		JPanel patientPanel = new JPanel(new GridLayout(5,1));
 		createPatientPanel(patientPanel);
 		
 		//Set up doctor panel
 		JPanel doctorPanel = new JPanel();
+		createDoctorPanel(doctorPanel);
 		
 		//Set up tabbed pane
 		JTabbedPane tbpane = new JTabbedPane();
@@ -37,6 +38,17 @@ public class UserInterface {
 		
 		patientPanel.add(new JLabel("I have a problem with my:"));
 		patientPanel.add(areasCB);
+		patientPanel.add(new JTextField());
+		patientPanel.add(new JButton("Find a doctor now!"));
+		patientPanel.add(new JLabel("insert dr diagnosis here"));
+	}
+	
+	private void createDoctorPanel(JPanel doctorPanel) {
+		String[] specialties = {"Skin", "Heart", "Chest", "Neck", "Throat", "Stomach"};
+		JComboBox specCB = new JComboBox(specialties);
+		
+		doctorPanel.add(new JLabel("My specialty is:"));
+		doctorPanel.add(specCB);
 	}
 	
 }

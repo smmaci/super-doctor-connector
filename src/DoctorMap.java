737f6807384583ad8c978439c84b5b5773ed130f
Adrian Kimmett <a.d.kimmett@gmail.com>
2016-12-01 12:51:38 -0500

    Added feature: find doctors by field and state

--- src/DoctorMap.java
+++ src/DoctorMap.java
@@ -4,22 +4,10 @@ import java.io.*;
 
 public class DoctorMap {
 	
-	protected HashMap<String, ArrayList<Doctor>> map;
+	protected ArrayList<Doctor> dlist;
 	
 	public DoctorMap() {
-		map = new HashMap<String, ArrayList<Doctor>>();
-		
-		/*
-		map.put("Leg", new Doctor("John", "123 Leg Rd", "555-555-5555", "leg"));
-		map.put("Arm", new Doctor("James", "456 Arm Ave", "111-111-1111", "arm"));
-		map.put("Head", new Doctor("Jim", "789 Head St", "222-222-2222", "head"));
-		map.put("Skin", new Doctor("Bob", "876 Skin Blvd", "123-456-789", "skin"));
-		map.put("Heart", new Doctor("Tom", "179 Heart Blvd", "941-232-343", "heart"));
-		map.put("Chest", new Doctor("Bilbo Baggins", "264 Chest Ave", "735-132-777", "chest"));
-		map.put("Neck", new Doctor("Frodo", "321 Neck St", "842-462-964", "neck"));
-		map.put("Throat", new Doctor("Gandalf", "394 Throat Blvd", "567-523-975", "throat"));
-		map.put("Stomach", new Doctor("Harry Potter", "953 Stomach St", "123-345-678", "stomach"));
-		*/
+		dlist = new ArrayList<Doctor>();
 		
 		String name, stAddr, city, state, zip, phone, field;
 		
@@ -38,9 +26,7 @@ public class DoctorMap {
 				phone = tokens.nextToken();
 				field = tokens.nextToken().toLowerCase();
 				
-				if(map.get(field) == null)
-					map.put(field, new ArrayList<Doctor>());
-				map.get(field).add(new Doctor(name, stAddr, city, state, zip, phone, field));
+				dlist.add(new Doctor(name, stAddr, city, state, zip, phone, field));
 				
 				line = bfr.readLine();
 			}
@@ -54,11 +40,12 @@ public class DoctorMap {
 
 	}
 	
-	public void addDoctor(String name, String stAddress, String city, String state, String zip, String number, String field){
+	public void addDoctor(Doctor d){
+		dlist.add(d);
 		BufferedWriter bfw = null;
 		try {
 			bfw = new BufferedWriter(new FileWriter("doctors.txt", true));
-			bfw.write("\n" + name + ";" + stAddress + ";" + city + ";" + state + ";" + zip + ";" + number + ";" + field);
+			bfw.write("\n" + d.getName() + ";" + d.getStreetAddress() + ";" + d.getCity() + ";" + d.getState() + ";" + d.getZip() + ";" + d.getPhoneNumber() + ";" + d.getField());
 		} catch (IOException e) {
 			System.err.println("ERROR: " + e.getMessage());
 		}
@@ -71,5 +58,26 @@ public class DoctorMap {
     		}
     	}        
 	} 
+	
+	public Doctor findDoctor(String field, String state) {
+		Doctor closeDr = new Doctor();
+		for(int i = 0; i < dlist.size(); i++) {
+			Doctor d = dlist.get(i);
+			if((d.getField().equals(field)) && (d.getState().equals(state))) {
+				closeDr = new Doctor(d);
+			}
+		}
+
+		return closeDr;
+	}
+	
+	public String toString() {
+		String s = "";
+		for(int i = 0; i < dlist.size(); i++) {
+			Doctor d = dlist.get(i);
+			s += d.toString();
+		}
+		return s;
+	}
 
 }
